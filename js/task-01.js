423f73c00c1a341671de19d1deae3370e8f8280a
sps-pashka-it <stetsyuk.p.s@gmail.com>
2023-02-08 20:01:20 +0200

    corrected task 1 and task 9

--- js/task-01.js
+++ js/task-01.js
@@ -1,27 +1,9 @@
 const categories = document.querySelector('#categories');
-console.log(`Number of categories: ${categories.children.length}`);
-
-// const totalCategories = document.querySelectorAll('.item');
-// console.log(`Number of categories: ${totalCategories.length}`);
-
-// console.log(categories);
-const categoriesfirstItem = categories.firstElementChild;
-console.log(`Category: ${categoriesfirstItem.querySelector('h2').textContent}`);
-console.log(`Elements: ${categoriesfirstItem.querySelector('ul').children.length}`);
-console.log(`Category: ${categoriesfirstItem.nextElementSibling.querySelector('h2').textContent}`);
-console.log(`Elements: ${categoriesfirstItem.nextElementSibling.querySelector('ul').children.length}`);
-console.log(`Category: ${categories.lastElementChild.querySelector('h2').textContent}`);
-console.log(`Elements: ${categories.lastElementChild.querySelector('ul').children.length}`);
-// console.log(categoriesItem.length);
 
 const listOfCategory = document.querySelector("#categories");
 const listOfClassItem = listOfCategory.querySelectorAll(".item");
 console.log(`Number of categories: ${listOfClassItem.length}`);
 
-listOfClassItem.forEach((item) => {
-    console.log(`Category: ${item.firstElementChild.textContent}`);
-    console.log(`Elements: ${item.lastElementChild.children.length}`)
-});
 
 const list = [...listOfClassItem].map((item) => {
     console.log(`Category: ${item.firstElementChild.textContent}`);
--- js/task-09.js
+++ js/task-09.js
@@ -1,12 +1,13 @@
 const getRandomHexColor = () => {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 };
 const body = document.querySelector('body');
 const btn = document.querySelector('.change-color');
 const nameColor = document.querySelector('.color');
 
 const changeColor = () => {
-  body.style.background = getRandomHexColor();
-  nameColor.textContent = getRandomHexColor();
+  const color = getRandomHexColor();
+  body.style.background = color;
+  console.log(nameColor.textContent = color);;
 }
 btn.addEventListener('click', changeColor);
